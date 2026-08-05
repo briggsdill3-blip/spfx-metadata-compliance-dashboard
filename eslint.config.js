ce3edf39656b4cb27c06f36f116e8349ace55d6a
Briggs, Dillon B CTR USARMY CPE OFFENSIVE FIRES (USA) <dillon.b.briggs.ctr@army.mil>
2026-08-05 08:10:37 -0400

    Add SonarJS plugin to eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,9 @@
 const spfxProfile = require('@microsoft/eslint-config-spfx/lib/flat-profiles/react');
+const sonarjs = require('eslint-plugin-sonarjs');
 
 module.exports = [
   ...spfxProfile,
+  sonarjs.configs.recommended,
   {
     files: ['**/*.ts', '**/*.tsx'],
     languageOptions: {
