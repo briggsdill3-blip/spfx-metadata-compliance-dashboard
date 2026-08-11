e2bd72b07f49e43855afc990ba5b51348cdaec07
Briggs, Dillon B CTR USARMY CPE OFFENSIVE FIRES (USA) <dillon.b.briggs.ctr@army.mil>
2026-08-11 12:00:15 -0400

    Fix props interface to actually include targetSites and excludedLibraries

--- src/webparts/metadataCompliance/components/IMetadataComplianceProps.ts
+++ src/webparts/metadataCompliance/components/IMetadataComplianceProps.ts
@@ -1,13 +1,15 @@
 import { SPFI } from '@pnp/sp';
 import { IReadonlyTheme } from '@microsoft/sp-component-base';
+import { ISiteEntry } from './ISiteEntry';
 
 export interface IMetadataComplianceProps {
   theme: IReadonlyTheme | undefined;
   environmentMessage: string;
   userDisplayName: string;
   sp: SPFI;
-  lockedLibrary: string;
+  targetSites: ISiteEntry[];
+  excludedLibraries: string[];
+  excludedFields: string[];
   goodThreshold: number;
   warnThreshold: number;
-  excludedFields: string[];
 }
